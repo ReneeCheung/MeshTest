a95d80d4f7bcaf1e09c8799761ef9837f1ddfe42
Renee Cheung <renee@CheungKas-MacBook-Pro.local>
2016-05-05 11:13:47 +0800

    modify wifi_setup

--- setup/wifi_setup.sh
+++ setup/wifi_setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rpi-update
 apt-get install curl build-essential
+
 ls -l /lib/modules
+
 FIRMWARE_REV=`cat /boot/.firmware_revision`
 KERNEL_REV=`curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/git_hash`
+
 curl -L https://github.com/Hexxeh/rpi-firmware/raw/${FIRMWARE_REV}/Module7.symvers >Module7.symver
 curl -L https://github.com/raspberrypi/linux/archive/${KERNEL_REV}.tar.gz >rpi-linux.tar.gz
 
@@ -11,6 +15,7 @@ cd /usr/src
 mkdir rpi-linux
 cd rpi-linux
 tar --strip-components 1 -xf /home/pi/rpi-linux.tar.gz
+
 modprobe configs
 gunzip -c /proc/config.gz >.config
 cp $HOME/Module7.symvers Module.symvers
@@ -20,4 +25,8 @@ ln -sv /usr/src/rpi-linux /lib/modules/$(uname -r)/build
 
 cd /home/pi/MeshTest/
 make
+mkdir /lib/modules/`uname -r`/kernel/drivers/8812au
+cp 8812au.ko /lib/modules/`uname -r`/kernel/drivers/8812au
+
+cat "8812au" >> /etc/modules
 
